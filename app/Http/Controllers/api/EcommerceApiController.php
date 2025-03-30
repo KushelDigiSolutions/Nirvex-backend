d7a7d482fe6b61af096c2a2f958c1c0a01de9f27
Aashish <webmasterashishgaur@gmail.com>
2025-03-30 23:34:54 +0530

    order history

--- app/Http/Controllers/api/EcommerceApiController.php
+++ app/Http/Controllers/api/EcommerceApiController.php
@@ -1860,7 +1860,7 @@ public function orderHistory(Request $request)
                 'status' => $orderStatusMapping[$order->order_status] ?? 'Unknown Status',
                 'item_count' => $order->order_items_count,
                 'total_amount' => $order->grand_total,
-                'created_at' => Carbon::parse($order->created_at)->diffForHumans(),
+                'created_at' => $order->created_at,
                 'order_items' => $order->orderItems->map(function ($item) {
                     return [
                         'product_name' => $item->product->name,
